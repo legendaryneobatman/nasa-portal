ddd372d034b2b2fb2c915c163bc38f2dde59d345
Alexandr <legendaryneobatman@gmail.com>
2022-07-25 19:31:59 +0500

    transfer project from vite

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="header">
+    <span class="header__title">
+      {{ title }}
+    </span>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageHeader',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.header {
+  @include font-h1();
+
+  position: sticky;
+  top: 0;
+  display: flex;
+  align-items: center;
+  height: 64px;
+  width: 100%;
+  box-shadow: 1px 8px 9px 0px rgba(34, 60, 80, 0.14);
+
+  &__title {
+    display: flex;
+    margin: 16px 24px;
+    background: $bg-primary;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <span>Welcome to NASA portal</span>
+    <p>The site that shows you cool stuff about nasa</p>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainPage'
+})
+</script>
+
+<style scoped></style>
